a7f49ce63c9ee5aac5b5361c9e863f71cb0a563c
Manoel Pereira de Queiroz <mpq.dev@pm.me>
2025-01-14 17:57:41 -0300

    :building_construction: Persist databases with MySQL

--- iv_onboarding/infra/app.py
+++ iv_onboarding/infra/app.py
@@ -3,10 +3,19 @@ import connexion
 from connexion.resolver import RestyResolver
 from flask_cors import CORS
 from models.db import db
+import os
 
-def create_app() -> connexion: 
+def create_app() -> connexion:
     app: connexion = connexion.App(__name__, specification_dir='../documentation/')
     app.add_api('user_service.yml', resolver=RestyResolver('api'))
     CORS(app.app,resources=r'/api/*',methods=['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'])
-    db.create_all()
+
+    app.app.config['SQLALCHEMY_DATABASE_URI'] = os.environ('DATABASE_URL')
+    app.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app.app)
+
+    with app.app.app.context():
+        db.create_all()
+
     return app
